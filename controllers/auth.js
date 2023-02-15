9db634d0a031576ded09101edfd95003cabba9a6
Pablo <pablolega7@gmail.com>
2023-02-15 20:42:24 +0100

    Start Create Backend CalendarApp

---
+++ controllers/auth.js
@@ -0,0 +1,37 @@
+const { response } = require("express");
+
+const createUser = ( req, res = response ) => {
+  const { name, email, password } = req.body;
+
+  res.status( 201 ).json({
+    ok  : true,
+    msg : "Register",
+    name,
+    email,
+    password,
+  });
+};
+
+const loginUser = (req, res = response) => {
+  const { email, password } = req.body;
+
+  res.status( 201 ).json({
+    ok  : true,
+    msg : "Login",
+    email,
+    password,
+  });
+};
+
+const renewToken = (req, res = response) => {
+  res.json({
+    ok  : true,
+    msg : "Renew",
+  });
+};
+
+module.exports = {
+  createUser,
+  loginUser,
+  renewToken,
+};
---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+/* Routes Users
+   host + /api/auth
+ */
+
+const express                               = require( 'express' );
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
+const router                                = express.Router();
+const { check }                             = require( 'express-validator' );
+const { validateFiled }                     = require('../middlewares/validate-fields');
+
+router.post(
+    '/new',
+    [
+        check( 'name', 'The Name is Required ' ).not().isEmpty(),
+        check( 'email', 'The Email is Required' ).isEmail(),
+        check( 'password', 'The password must be Six Characters' ).isLength({ min: 6 }),
+        validateFiled
+    ],
+    createUser
+);
+
+router.post(
+    '/' ,
+    [
+        check( 'email', 'The Email is Required' ).isEmail(),
+        check( 'password', 'The password must be Six Characters' ).isLength({ min: 6 }),
+        validateFiled
+    ],
+ loginUser
+);
+
+router.get( '/renew' , renewToken );
+
+module.exports = router;
