75e18935ea70fbf4036db0c4a85216317a18ea95
Pablo <pablolega7@gmail.com>
2023-02-18 13:35:06 +0100

    Add Cors in App

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
 const express          = require('express');
 const { dbConnection } = require('./database/config');
+const cors             = require('cors');
 require( 'dotenv' ).config();
 
 const app = express();
 
 dbConnection();
+
+app.use(cors());
 
 app.use( express.json() );
 
